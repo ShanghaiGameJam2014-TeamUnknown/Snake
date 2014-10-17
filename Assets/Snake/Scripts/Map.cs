693340e6339b7b2effe48cfb426942c12c9f6004
AquarHEAD Lou <aquarhead@gmail.com>
2014-10-17 21:24:09 +0800

    Map init.

--- Assets/Snake/Scripts/Map.cs
+++ Assets/Snake/Scripts/Map.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 
 public class Map : MonoBehaviour {
-
+	
 	public int MapSize;
+	public GameObject TilePrefab;
 
-	private GameObject[,] Map;
+	private GameObject[,] MapTiles;
 
 	// Use this for initialization
 	void Start () {
-		Map = new GameObject[32, 32];
+		for (int i=0; i<MapSize; i++) {
+			for (int j=0; j<MapSize; j++) {
+				MapTiles[i, j] = Instantiate(TilePrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+				MapTiles[i, j].GetComponent<UnitTile>().UpdatePosition();
+				MapTiles[i, j].SetActive(true);
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
