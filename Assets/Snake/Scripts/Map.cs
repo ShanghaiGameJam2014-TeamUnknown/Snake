25e37acb4629ac29451973f0e7d311c0127dd116
AquarHEAD Lou <aquarhead@gmail.com>
2014-10-17 21:03:30 +0800

    Init map functions.

--- Assets/Snake/Scripts/Map.cs
+++ Assets/Snake/Scripts/Map.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapScript : MonoBehaviour {
+public class Map : MonoBehaviour {
+
+	public int MapSize;
+
+	private GameObject[,] Map;
 
 	// Use this for initialization
 	void Start () {
-	
+		Map = new GameObject[32, 32];
 	}
 	
 	// Update is called once per frame
