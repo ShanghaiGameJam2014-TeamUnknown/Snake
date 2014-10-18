9e622a79834b8ead997a58afd3e443a3ac0858cf
Ziqin <Ziqin.Qu@gmail.com>
2014-10-18 15:00:17 +0800

    Add crash to boss

--- Assets/Snake/Scripts/BossGM.cs
+++ Assets/Snake/Scripts/BossGM.cs
@@ -7,21 +7,56 @@ public class BossGM : MonoBehaviour {
 	public List<GameObject> SoldierPrefabs;
 	public GameObject Boss;
 
+	public int OffsetX;
+	public int Speed;
+	public Vector3 v;
+	public Vector3 SoldierInitPos;
+
+	private int FrameCount;
+
+	private GameObject test;
+	
 	// Use this for initialization
 	void Start () {
-		Debug.Log(CommanData.CommonSnake.Count);
-		Debug.Log((int)CommanData.CommonSnake[0]);
-		GameObject test = Instantiate(SoldierPrefabs[(int)CommanData.CommonSnake[0] - 1], new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		//Debug.Log(CommanData.CommonSnake.Count);
+		//Debug.Log((int)CommanData.CommonSnake[0]);
+		test = Instantiate(SoldierPrefabs[0], SoldierInitPos, Quaternion.identity) as GameObject;
 		test.SetActive(true);
-
-
-		for(int i=0; i<CommanData.CommonSnake.Count;i++)
-		{
-			AudioManager.instance.sequence.Add((int)CommanData.CommonSnake[i]);
-		}
+		test.transform.localScale = new Vector3(1.4f, 1.4f, 1.4f);
+		test.GetComponent<Animator>().SetTrigger("walk_right");
+		//for(int i=0; i<CommanData.CommonSnake.Count;i++)
+		//{
+		//	AudioManager.instance.sequence.Add((int)CommanData.CommonSnake[i]);
+		//}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		if(FrameCount<=Speed)
+		{
+			FrameCount++;
+		}
+		else
+		{
+			//first test if fail
+
+			//else
+				CrashToBoss(test);
+			// test if win
+
+			FrameCount = 0;
+		}
+	}
+
+	void CrashToBoss(GameObject tile)
+	{
+		tile.transform.position += v;
+
+		if(tile.transform.position.x >= Boss.transform.position.x + OffsetX)
+		{
+			//tile.settrigger()
+			Debug.Log("Dead");
+		}
 	}
 }
